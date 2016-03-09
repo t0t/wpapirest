08ac6c829db4589a3c457a6849b4aea6248cf774
Sergio Forés Raga <sergiofores@gmail.com>
2016-03-09 12:11:35 +0100

    init :pray:

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The main theme file template.
+ */
+?>
+
+<!DOCTYPE html>
+
+<html lang="es">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>WP API REST</title>
+	</head>
+
+	<body>
+
+		<header>WP API REST</header>
+
+		<main id="main"></main>
+
+	</body>
+
+  <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/build.js"></script>
+
+</html>
---
+++ js/build.js
@@ -0,0 +1,7 @@
+
+var mainDiv = document.getElementById('main');
+var posts = "http://dev.wpapirest.com/wp-json/wp/v2/posts";
+
+console.log('Hello world');
+console.log(posts);
+console.log(mainDiv);
