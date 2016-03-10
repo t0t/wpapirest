0a9b9ce2f890d36e348a6290d18ef73104e725df
Sergio Forés Raga <sergiofores@gmail.com>
2016-03-10 13:27:41 +0100

    XHR ES6 Class :scream:

--- assets/js/app/app.js
+++ assets/js/app/app.js
@@ -9,91 +9,34 @@ class Xhr {
   }
 
   get json() {
-    let data = [];
+
     let xhr = new XMLHttpRequest();
     xhr.open( 'GET', this.url );
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = jsonGot;
+    xhr.send();
+
+    function jsonGot() {
       if (xhr.readyState === 4) {
-        data = JSON.parse(xhr.responseText);
-        console.log(data);
+        if (xhr.status === 200) {
+          let json = JSON.parse(xhr.responseText);
+          console.log(json);
+          return json;
+        }
       }
     }
-    xhr.send();
-    return data;
+
   }
+
 }
 
 // CONTROLADOR
 let posts = new Xhr( 'posts' );
-console.log(posts.json);
-// let iteratePosts = posts.json;
-// for (var i = 0; i < iteratePosts.length; i++) {
-//   console.log(iteratePosts[i].title);
-// }
-// for (post of iteratePosts) {
-//   console.log(post.title);
-// }
+// let p = posts.json;
+console.log(posts);
+console.log(posts.data);
+// console.log(posts.json(this.data));
 
-// let pages = new Xhr( 'pages' );
-// console.log( pages.json );
-
-// let pages = new Xhr( 'pages' );
-// console.log( pages.url );
-// console.log( `Paginas: ${pages.api}` );
-
-// MODEL
-// let baseUrl = "http://dev.wpapirest.com/wp-json/wp/v2/";
-// let postsUrl = baseUrl + "posts";
-//
-// let xhr = new XMLHttpRequest();
-//
-// function getPosts () {
-//   if (this.readyState === 4) {
-//     let posts = JSON.parse(this.responseText);
-//     for (var i = 0; i < posts.length; i++) {
-//       console.log(posts[i].title);
-//       console.log(posts[i].content);
-//     }
-//   }
-// }
-//
-// xhr.addEventListener( "readystatechange", getPosts );
-// xhr.open( "GET", postsUrl );
-// xhr.send();
-
-// class Xhr {
-//   constructor (name) {
-//     this.name = name;
-//     this.baseUrl = "http://dev.wpapirest.com/wp-json/wp/v2/";
-//     this.url = this.baseUrl+this.name;
-//     this.xhr = new XMLHttpRequest();
-//   }
-//
-//   get() {
-//     if (this.readyState === 4) {
-//       let posts = JSON.parse(this.responseText);
-//       for (var i = 0; i < posts.length; i++) {
-//         console.log(posts[i].title);
-//         console.log(posts[i].content);
-//       }
-//     }
-//   }
-//
-// }
-//
-// let posts = new Xhr('posts');
-// posts.xhr.addEventListener( "readystatechange", posts.get() );
-// posts.xhr.open( "GET", posts.url );
-// posts.xhr.send();
-
-// console.log(posts.get());
-//
-// let pages = new Xhr('pages');
-// console.log(pages.url);
-
-// CONTROLLER
-
-// VIEW
+// VISTA
 let elMain = document.getElementById('main');
 let elDiv = document.createElement('div');
 elDiv.innerHTML = " Loremm";
--- assets/js/build.js
+++ assets/js/build.js
@@ -32,17 +32,21 @@ var Xhr = function () {
   _createClass(Xhr, [{
     key: 'json',
     get: function get() {
-      var data = [];
+
       var xhr = new XMLHttpRequest();
       xhr.open('GET', this.url);
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-          data = JSON.parse(xhr.responseText);
-          console.log(data);
-        }
-      };
+      xhr.onreadystatechange = jsonGot;
       xhr.send();
-      return data;
+
+      function jsonGot() {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            var json = JSON.parse(xhr.responseText);
+            console.log(json);
+            return json;
+          }
+        }
+      }
     }
   }]);
 
@@ -52,75 +56,12 @@ var Xhr = function () {
 // CONTROLADOR
 
 var posts = new Xhr('posts');
-console.log(posts.json);
-// let iteratePosts = posts.json;
-// for (var i = 0; i < iteratePosts.length; i++) {
-//   console.log(iteratePosts[i].title);
-// }
-// for (post of iteratePosts) {
-//   console.log(post.title);
-// }
+// let p = posts.json;
+console.log(posts);
+console.log(posts.data);
+// console.log(posts.json(this.data));
 
-// let pages = new Xhr( 'pages' );
-// console.log( pages.json );
-
-// let pages = new Xhr( 'pages' );
-// console.log( pages.url );
-// console.log( `Paginas: ${pages.api}` );
-
-// MODEL
-// let baseUrl = "http://dev.wpapirest.com/wp-json/wp/v2/";
-// let postsUrl = baseUrl + "posts";
-//
-// let xhr = new XMLHttpRequest();
-//
-// function getPosts () {
-//   if (this.readyState === 4) {
-//     let posts = JSON.parse(this.responseText);
-//     for (var i = 0; i < posts.length; i++) {
-//       console.log(posts[i].title);
-//       console.log(posts[i].content);
-//     }
-//   }
-// }
-//
-// xhr.addEventListener( "readystatechange", getPosts );
-// xhr.open( "GET", postsUrl );
-// xhr.send();
-
-// class Xhr {
-//   constructor (name) {
-//     this.name = name;
-//     this.baseUrl = "http://dev.wpapirest.com/wp-json/wp/v2/";
-//     this.url = this.baseUrl+this.name;
-//     this.xhr = new XMLHttpRequest();
-//   }
-//
-//   get() {
-//     if (this.readyState === 4) {
-//       let posts = JSON.parse(this.responseText);
-//       for (var i = 0; i < posts.length; i++) {
-//         console.log(posts[i].title);
-//         console.log(posts[i].content);
-//       }
-//     }
-//   }
-//
-// }
-//
-// let posts = new Xhr('posts');
-// posts.xhr.addEventListener( "readystatechange", posts.get() );
-// posts.xhr.open( "GET", posts.url );
-// posts.xhr.send();
-
-// console.log(posts.get());
-//
-// let pages = new Xhr('pages');
-// console.log(pages.url);
-
-// CONTROLLER
-
-// VIEW
+// VISTA
 var elMain = document.getElementById('main');
 var elDiv = document.createElement('div');
 elDiv.innerHTML = " Loremm";
