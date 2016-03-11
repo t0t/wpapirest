bec59de8e9118ee91fe21ca82215284c851815d1
Sergio Forés Raga <sergiofores@gmail.com>
2016-03-11 13:50:13 +0100

    Configuracion webpack

---
+++ src/components/card/index.js
@@ -0,0 +1,26 @@
+import {Image, Div, Text, HtmlComponent} from '../html';
+
+class Card extends HtmlComponent {
+  constructor({name, image} = {}){
+    super({name, image});
+    this._name = name;
+    this._image = image;
+  }
+
+  render() {
+    const card = new Div('Card');
+    const cardContent = new Div('Card-content');
+    const title = new Div('Card-title');
+    const image = new Image({className: 'Card-image', src: this._image});
+    const name = new Text({text: this._name});
+
+    title.appendChild(name);
+    cardContent.appendChild(title);
+    card.appendChild(image);
+    card.appendChild(cardContent);
+    
+    return card.render();
+  }
+};
+
+export default Card;
---
+++ src/components/html/index.js
@@ -0,0 +1,6 @@
+import Image from './image';
+import Div from './div';
+import Text from './text';
+import HtmlComponent from './htmlComponent';
+
+export {Image, Div, Text, HtmlComponent};
